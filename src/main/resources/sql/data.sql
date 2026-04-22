72c1fd4676fc55330548749a59be4afad4204f44
TambyGit <hei.tamby@gmail.com>
2026-04-22 09:19:59 +0300

    update: OAS version 0.0.2 with additional features

--- src/main/resources/sql/data.sql
+++
@@ -1,8 +0,0 @@
-CREATE
-DATABASE agricultural_db;
-CREATE
-USER agricultural_user WITH PASSWORD '123456';
-GRANT ALL PRIVILEGES ON DATABASE
-agricultural_db TO agricultural_user;
-GRANT ALL PRIVILEGES ON SCHEMA
-PUBLIC TO agricultural_user;
